23c4d6fee200de421da23b9c109c2d4017b673b4
Cody Duval <cody.duval@gmail.com>
2018-03-01 11:43:16 -0500

    Fetch cf cli from s3

--- .circleci/cloudfoundry.sh
+++ .circleci/cloudfoundry.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # ...first add the Cloud Foundry Foundation public key and package repository to your system
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-# ...then, update your local package index, then finally install the cf CLI
-sudo apt-get update
-sudo apt-get install cf-cli
-cf login -a https://api.run.pivotal.io -u ${CF_EMAIL} -p ${CF_PASSWORD} -o
+
+# Use the URL to a Debian 64 bit installer select from here:
+# https://github.com/cloudfoundry/cli/releases
+# This is the source file after following the redirect
+wget https://s3.amazonaws.com/go-cli/releases/v6.12.4/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
+
+rm temp.deb
+
+cf api $CF_API
+cf login --u $CF_USERNAME --p $CF_PASSWORD --o $CF_ORGANIZATION --s $CF_SPACE
